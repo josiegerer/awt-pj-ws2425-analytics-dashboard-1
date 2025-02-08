25357e18429589454f71c51195371868aae7c410
Samy <samy.hamdad@campus.tu-berlin.de>
2025-02-08 16:55:51 +0100

    bug fix for last seven days

--- xapi_statementgenerator/save_data_in_lrs.py
+++ xapi_statementgenerator/save_data_in_lrs.py
@@ -99,4 +99,4 @@ def send_data_to_lrs(json_file_path, lrs_url, base_url, username, password):
     print("User emails saved to user_emails.txt")
     
 
-send_data_to_lrs("./generated_data/new_xapi_statements_12userdata.json", "http://localhost:8080/xapi/statements", "http://localhost:8080", "my_username", "my_password")
+send_data_to_lrs("./generated_data/07022025_xapi_statements_combined.json", "http://localhost:8080/xapi/statements", "http://localhost:8080", "my_username", "my_password")
